3d4d1b11b9aff9d9be5dd3c87fff29b2aed2ac8f
Rora Alem <rora.goitom@gmail.com>
2023-09-29 04:26:04 -0400

    Create transition function to update mode state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-
+  function transition(newMode) {
+    setMode(newMode);
+  }
   return { mode };
 }
+
