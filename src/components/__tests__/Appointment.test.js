319511811a70d0d0bd025066dd07e59edf274c9b
Rora Alem <rora.goitom@gmail.com>
2023-10-05 11:13:17 -0400

    Replace test with promise chain waitForElement

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,22 +7,34 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, waitForElement,fireEvent } from "@testing-library/react";
 
 /*
   We import the component that we are testing
 */
-import Appointment from "components/Appointment";
+import Application from "components/Application";
 
 /*
   A test that renders a React Component
 */
 describe("Appointment", () => {
-  //const{ debug } = render();
-  it("renders without crashing", () => {
-    render(<Appointment />);
-     //debug();
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+
+    //create fucntion 'getByText' to query and interact with elements rendered in the component
+    const { getByText } = render(<Application/>);
+
+    // ensuring that the "Monday" element is present in the rendered component before proceeding with the test.
+    // use 'waitForElement' func to wait until "Monday" element is available, once avaialbel continues
+    return waitForElement(() => getByText("Monday")).then(() => {
+
+      //click event on the element"Tuesday", used to change the selected day from Monday to Tuesday in appointment schedule
+      fireEvent.click(getByText("Tuesday"));
+
+      //assertion - checks whether an element with text'Leopold Silvers' is present in the component after click event
+      // If it's - it'll pass otherwise -it'll fail
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 
- 
 });
