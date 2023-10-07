3c635b4097ba790c8729fd86a06518978913ab09
Rora Alem <rora.goitom@gmail.com>
2023-10-06 20:20:45 -0400

    Refactor test to use a single command chain

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,16 +3,14 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 
-  it("should navigate to Tuesday", () =>{
+  it("should navigate to Tuesday", () => {
     //visit home page
     cy.visit("/");
 
-    //find an element containing the text "Tuesday"
-    cy.get("li").contains("Tuesday").click();
-    //click it
-
-    //assertion to confirm that the list item element that contains the text "Tuesday" is selected.
+    //Refactor the test to use a single command chain that finds the list item,
+    // clicks it and checks it for the correct background colour.
     cy.contains("li", "Tuesday")
-  .should("have.css", "background-color", "rgb(242, 242, 242)");
-  })
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
 });
